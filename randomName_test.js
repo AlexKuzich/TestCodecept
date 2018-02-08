98f08a747ad875e377fb139c2cfc633b83a3098c
Alex Kuzich <akuzich@klika-tech.com>
2018-02-08 21:18:20 +0300

    Codeceptjs updates

--- randomName_test.js
+++ randomName_test.js
@@ -1,7 +1,10 @@
+let mailOperaions = require('./quickstart.js');
+
 Feature('Mailing Service');
 
 Scenario('Check if mails are sent once 9 hours', (I) => {
-
+    I.amOnPage('http://www.protractortest.org/#/');
+    console.log(mailOperaions.listMessages());
 });
 
 
